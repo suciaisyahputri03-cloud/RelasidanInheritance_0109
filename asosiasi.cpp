bf1f3f610a3d4cf51b900edd52a616e432361063
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-05-12 19:13:08 +0700

    Menambahkan method tambahDokter dan cetakDokter

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -10,3 +10,9 @@ public:
     pasien(string pNama) :nama(pNama){
         cout << "Pasien \"" << nama << "\" ada";
     }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();
+};
