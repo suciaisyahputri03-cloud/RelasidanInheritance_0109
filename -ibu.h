bd5fe506107dc433f2ffdca1346281ba7250140b
suciaisyahputri03-cloud <suciaisyahputri03@gmail.com>
2026-05-12 18:54:49 +0700

    implementasi class ibu dengan vektor daftar anak

--- -ibu.h
+++ -ibu.h
@@ -4,4 +4,15 @@
 
 class ibu {
 public:
-}
+    string nama;
+    vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \ "" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambah Anakk(anak*);
+    void cetakAnak();
+};
